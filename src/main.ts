96b5ac459179296224c1032a6188c2b929aaa365
Sergio Zaldivar <zayser99@hotmail.com>
2026-07-30 23:25:28 -0600

    feat(backend) correcciones para build

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://zayser-pos.vercel.app', 'https://trenzasyconchasmexicanas.store', 'https://api.trenzasyconchasmexicanas.store'],
     credentials: true,
   });
 
